6b85eb64c4f90097e43b0e65df72d4262a21716d
kanfa <kanfaray@yahoo.com>
2017-03-13 11:13:14 +0000

    Add job table

--- resource/sql/userdb.sql
+++ resource/sql/userdb.sql
@@ -1,3 +1,12 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Lun 13 Mars 2017 à 11:12
+-- Version du serveur :  5.7.9
+-- Version de PHP :  5.6.16
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -20,41 +29,26 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'User Id',
+  `firstname` varchar(150) NOT NULL,
+  `lastname` varchar(150) NOT NULL,
   `username` varchar(150) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `last_connection` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `online` tinyint(1) NOT NULL,
   `group_id` int(10) UNSIGNED NOT NULL,
+  `job_id` int(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_us_gr` (`group_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  KEY `fk_us_gr` (`group_id`),
+  KEY `fk_usersjob` (`job_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `online`, `group_id`) VALUES
-  (1, 'admin', '21232f297a57fa5fa743f894ae4af801ffc3', 0, 1);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `user_group`
---
-
-DROP TABLE IF EXISTS `user_group`;
-CREATE TABLE IF NOT EXISTS `user_group` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'User Group Id',
-  `gr_name` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `gr_unique` (`gr_name`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Contenu de la table `user_group`
---
-
-INSERT INTO `user_group` (`id`, `gr_name`) VALUES
-  (1, 'admin');
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `password`, `last_connection`, `created`, `online`, `group_id`, `job_id`) VALUES
+  (1, 'Ibrahim', 'Maiga', 'admin', '21232f297a57fa5fa743f894ae4af801ffc3', '2017-02-15 11:23:00', '2017-02-15 11:44:29', 0, 1, 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
